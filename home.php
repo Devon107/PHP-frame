09c90c0c3f28d3106f273822092a30c8ee32b9ae
Skenji <marvin.vasquez@hotmail.es>
2018-11-23 17:17:56 -0600

    home file

---
+++ home.php
@@ -0,0 +1,19 @@
+<Doctype!>
+<html>
+    <head>
+        <meta/>
+        <link rel="stylesheet" href=""/>
+        <title><?php echo APP_NAME; ?></title>
+    </head>
+    <body>
+        <header>
+        </header>
+        <main>
+            <?php echo "Welcome"; ?>
+        </main>
+        <footer>
+            <script type="text/javascript">
+            </script>
+        </footer>
+    </body>
+</html>
